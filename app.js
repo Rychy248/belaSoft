bee1383198680290650f230a6f84b8a2205db661
Rychy <jorgeajrhh@gmail.com>
2023-06-06 00:30:43 -0600

    app 2 added

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const bodyParser = require("body-parser");
 // const ejs = require("ejs");
 // const mongoose = require('mongoose');
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
 }));
+
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
@@ -30,98 +34,12 @@ app.route("/articles")
 
 .get(function(req, res){
 
-  res.json({
+  res.json(
+    {
     "data":"HEllo World",
     "product":{a:"asdf",b:"asdf"}
   })
-})
-
-.post(function(req, res){
-
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  newArticle.save(function(err){
-    if (!err){
-      res.send("Successfully added a new article.");
-    } else {
-      res.send(err);
-    }
-  });
-})
-
-.delete(function(req, res){
-
-  Article.deleteMany(function(err){
-    if (!err){
-      res.send("Successfully deleted all articles.");
-    } else {
-      res.send(err);
-    }
-  });
 });
-
-////////////////////////////////Requests Targetting A Specific Article////////////////////////
-
-app.route("/articles/:articleTitle")
-
-.get(function(req, res){
-
-  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
-    if (foundArticle) {
-      res.send(foundArticle);
-    } else {
-      res.send("No articles matching that title was found.");
-    }
-  });
-})
-
-.put(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {title: req.body.title, content: req.body.content},
-    {overwrite: true},
-    function(err){
-      if(!err){
-        res.send("Successfully updated the selected article.");
-      }
-    }
-  );
-})
-
-.patch(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {$set: req.body},
-    function(err){
-      if(!err){
-        res.send("Successfully updated article.");
-      } else {
-        res.send(err);
-      }
-    }
-  );
-})
-
-.delete(function(req, res){
-
-  Article.deleteOne(
-    {title: req.params.articleTitle},
-    function(err){
-      if (!err){
-        res.send("Successfully deleted the corresponding article.");
-      } else {
-        res.send(err);
-      }
-    }
-  );
-});
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
---
+++ app2/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+ 
+
+// View engine setup
+app.set('view engine', 'ejs');
+
+ 
+// Without middleware
+app.get('/', function (req, res) {
+ 
+    // Rendering home.ejs page
+    res.render('home');
+})
+ 
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
